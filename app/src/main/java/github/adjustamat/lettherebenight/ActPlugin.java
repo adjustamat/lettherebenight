7e1fda94000b1a5f9e5195f8192c65449e0e7d06
Martin Kvarnström <1641340+adjustamat@users.noreply.github.com>
2022-05-16 14:21:35 +0200

    Activities

---
+++ app/src/main/java/github/adjustamat/lettherebenight/ActPlugin.java
@@ -0,0 +1,14 @@
+package github.adjustamat.lettherebenight;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActPlugin extends AppCompatActivity
+{
+   
+   @Override
+   protected void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.act_plugin);
+   }
+}
---
+++ app/src/main/java/github/adjustamat/lettherebenight/ActSettings.java
@@ -0,0 +1,14 @@
+package github.adjustamat.lettherebenight;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActSettings extends AppCompatActivity
+{
+   
+   @Override
+   protected void onCreate(Bundle savedInstanceState){
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.act_settings);
+   }
+}
