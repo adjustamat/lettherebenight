b845812c150dfc5224abf04071ff5e8fa0e1aab8
Martin Kvarnström <1641340+adjustamat@users.noreply.github.com>
2022-05-16 15:10:09 +0200

    Gradle and icon

--- app/src/main/java/github/adjustamat/lettherebenight/ActPlugin.java
+++ app/src/main/java/github/adjustamat/lettherebenight/ActPlugin.java
@@ -1,14 +1,27 @@
 package github.adjustamat.lettherebenight;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 
-public class ActPlugin extends AppCompatActivity
+public class ActPlugin extends Activity implements OnTouchListener
 {
    
    @Override
    protected void onCreate(Bundle savedInstanceState){
       super.onCreate(savedInstanceState);
       setContentView(R.layout.act_plugin);
+   }
+   
+   @Override
+   public boolean onTouch(View view, MotionEvent event){
+      return false;
+   }
+   
+   @Override
+   public void onPointerCaptureChanged(boolean hasCapture){
+      super.onPointerCaptureChanged(hasCapture);
    }
 }
